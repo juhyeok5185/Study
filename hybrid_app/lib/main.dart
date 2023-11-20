c6764f19cd2e3114951d4acb5f17511cac7eec89
juhyeok5185 <qkrwngur5185@naver.com>
2023-11-20 20:33:27 +0900

    added

--- hybrid_app/lib/main.dart
+++ hybrid_app/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:webview_flutter/webview_flutter.dart';
 import 'package:webview_flutter_android/webview_flutter_android.dart';
 import 'package:webview_flutter_wkwebview/webview_flutter_wkwebview.dart';
 
+
+
 void main() {
   runApp(const MyApp());
 }
