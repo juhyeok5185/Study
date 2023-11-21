d9db8e8d4b75e4d322310e7d3d48a369f2377d68
juhyeok5185 <qkrwngur5185@naver.com>
2023-11-21 21:33:28 +0900

    added
    added

--- 김영한/스프링기본/core/src/main/java/hello/core/MemberApp.java
+++ 김영한/스프링기본/core/src/main/java/hello/core/MemberApp.java
@@ -4,16 +4,28 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        //스프링없이
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        //객체지향을 지키기위해
+//        //MemberService memberService = new MemberServiceImpl();
+
+        //스프링으로 변경해보기
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService" ,MemberService.class);
+
         Member member = new Member(1L , "mamberA" , Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
+
+
+
         System.out.println("findMember = " + findMember.getName());
         System.out.println("member.getName() = " + member.getName());
     }
