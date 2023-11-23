e1bebd9c2a8860f99c090e0a295f972e17ae49d3
juhyeok5185 <qkrwngur5185@naver.com>
2023-11-23 21:19:58 +0900

    added
    added

---
+++ CodingTest/src/Main.java
@@ -0,0 +1,8 @@
+import lv0.Lv0;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ CodingTest/src/lv0/Lv0.java
@@ -0,0 +1,35 @@
+package lv0;
+
+public class Lv0 {
+
+    public static int lv0_1(int[] arr1, int[] arr2){
+//        이 문제에서 두 정수 배열의 대소관계를 다음과 같이 정의합니다.
+//        두 배열의 길이가 다르다면, 배열의 길이가 긴 쪽이 더 큽니다.
+//        배열의 길이가 같다면 각 배열에 있는 모든 원소의 합을 비교하여
+//        다르다면 더 큰 쪽이 크고, 같다면 같습니다.
+//        두 정수 배열 arr1과 arr2가 주어질 때,
+//        위에서 정의한 배열의 대소관계에 대하여 arr2가 크다면 -1,
+//        arr1이 크다면 1, 두 배열이 같다면 0을 return 하는 solution 함수를 작성해 주세요.
+
+        int answer = 0;
+        if(arr1.length > arr2.length){
+            answer = 1;
+        } else if (arr1.length < arr2.length) {
+            answer = -1;
+        } else {
+            int arr1Int = 0;
+            int arr2Int = 0;
+            for (int i = 0; i < arr1.length; i++){
+                arr1Int += arr1[i];
+                arr2Int += arr2[i];
+            }
+            if(arr1Int > arr2Int){
+                answer = 1;
+            } else if(arr1Int < arr2Int){
+                answer = -1;
+            }
+        }
+
+        return answer;
+    }
+}
