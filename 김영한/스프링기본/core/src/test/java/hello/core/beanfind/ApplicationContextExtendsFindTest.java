13e6c76a513aa9e4a5598ba6451f463a7a2b9868
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2023-11-27 07:59:02 +0900

    added
    added

---
+++ 김영한/스프링기본/core/src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,35 @@
+package hello.core.beanfind;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class ApplicationContextExtendsFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+
+    @Test
+    @DisplayName("부모타입으로 조회시 자식이 둘 이상 있으면 , 중복오류가 발생한다.")
+    void findAllBean(){
+        DiscountPolicy beanDefinitionNames = ac.getBean(DiscountPolicy.class);
+
+    }
+
+    @Configuration
+    static class TestConfig{
+
+        @Bean
+        public DiscountPolicy rateDiscountPolicy(){
+            return new RateDiscountPolicy();
+        }
+
+        @Bean
+        public DiscountPolicy fixDiscountPolicy(){
+            return new FixDiscountPolicy();
+        }
+    }
+}
