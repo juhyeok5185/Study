6285351d173eb51f0890c04c264c06bf4ae13193
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2023-11-26 18:21:50 +0900

    added
    added

--- CodingTest/src/Main.java
+++ CodingTest/src/Main.java
@@ -2,7 +2,10 @@ import lv0.Lv0;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        int[] a = {1, 2, 3};
+        int[] b = {2, 4, 7};
+        int answer = 0;
+        answer = Lv0.lv0_1(a,b);
+        answer = Lv0.lv0_2(a , 2);
     }
 }
--- CodingTest/src/lv0/Lv0.java
+++ CodingTest/src/lv0/Lv0.java
@@ -32,4 +32,19 @@ public class Lv0 {
 
         return answer;
     }
+
+    public static int lv0_2(int[] num_list , int n){
+//        정수 리스트 num_list와 찾으려는 정수 n이 주어질 때,
+//        num_list안에 n이 있으면 1을 없으면 0을 return하도록
+//        solution 함수를 완성해주세요.
+        int answer = 0;
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] == n){
+                answer = 1;
+            }
+        }
+        return answer;
+    }
+
+
 }
