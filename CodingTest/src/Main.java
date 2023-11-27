2069797c36cf1f386165b8bbf6163eb4d92b9f26
juhyeok5185 <117324639+juhyeok5185@users.noreply.github.com>
2023-11-27 22:15:53 +0900

    added

--- CodingTest/src/Main.java
+++ CodingTest/src/Main.java
@@ -7,5 +7,9 @@ public class Main {
         int answer = 0;
         answer = Lv0.lv0_1(a,b);
         answer = Lv0.lv0_2(a , 2);
+        int[] answer2 = Lv0.lv0_3(2,10);
+        for (int i : answer2) {
+            System.out.println(i);
+        }
     }
 }
--- CodingTest/src/lv0/Lv0.java
+++ CodingTest/src/lv0/Lv0.java
@@ -46,5 +46,14 @@ public class Lv0 {
         return answer;
     }
 
+    public static int[] lv0_3(int start_num, int end_num){
+//        정수 start_num와 end_num가 주어질 때, start_num부터
+//        end_num까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
+        int[] answer = new int[end_num - start_num + 1];
+        for (int i = start_num, j = 0; i <= end_num; i++, j++) {
+            answer[j] = i;
+        }
+        return answer;
+    }
 
 }
